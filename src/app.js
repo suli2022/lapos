0194a514f883753b9b3745abf37f918d06019de9
Sallai András <joska@zold.and>
2023-02-14 18:50:10 +0100

    Törlés gomb hozzáadva

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const salaryInput = document.querySelector("#salary");
 const addButton = document.querySelector("#addButton");
 
 const dolgozoLista = [
-    { name: "Pali", city: "Szolnok", salary: 385 },
-    { name: "Kait", city: "Szolnok", salary: 320 },
-    { name: "Mari", city: "Szeged", salary: 395 },
-    { name: "Dani", city: "Szeged", salary: 401 },
-    { name: "Atti", city: "Miskolc", salary: 372 },
-    { name: "Pisti", city: "Szolnok", salary: 357 },
-    { name: "Géza", city: "Pécs", salary: 325 }
+    { id: 1, name: "Pali", city: "Szolnok", salary: 385 },
+    { id: 2, name: "Kait", city: "Szolnok", salary: 320 },
+    { id: 3, name: "Mari", city: "Szeged", salary: 395 },
+    { id: 4, name: "Dani", city: "Szeged", salary: 401 },
+    { id: 5, name: "Atti", city: "Miskolc", salary: 372 },
+    { id: 6, name: "Pisti", city: "Szolnok", salary: 357 },
+    { id: 7, name: "Géza", city: "Pécs", salary: 325 }
 ];
 
 function loadEmployees() {
@@ -29,10 +29,28 @@ function loadEmployees() {
         tr.append(tdName);
         tr.append(tdCity);
         tr.append(tdSalary);
+        tr.append(generateDeleteButton(dolgozo.id));
     });
 }
 
 loadEmployees();
+
+function generateDeleteButton(id) {
+    let tdDel = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Törlés";
+    button.classList = "btn btn-primary";
+    handleEvents(button, id);
+    tdDel.append(button);
+    return tdDel;
+}
+
+function handleEvents(button, id) {
+    button.addEventListener('click', () => {
+        console.log(id);
+    });
+}
+
 
 addButton.addEventListener('click', () => {
     console.log('működik');
